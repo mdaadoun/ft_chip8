09090b841eea976205826682cbfe4d0dfd950bd5
mdaadoun <daadounmichael@gmail.com>
2023-01-10 10:45:46 +0100

    assembly

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ void help() {
         << "\t./chip8 <rom_path> : run the rom.\n\n"
         << "\t./chip8 -b, --bytes <rom_path> :\n\t\t dump the rom bytes.\n\n"
         << "\t./chip8 -d, --disassembly <rom_path> :\n\t\t disassembly the rom, copy content in file <rom_path>.dis.txt\n\n"
-        << "\t./chip8 -a, --assembly <source_path> :\n\t\t assembly the text file at <source_path> as a binary rom <source_path>.ch8\n\n"
+//        << "\t./chip8 -a, --assembly <source_path> :\n\t\t assembly the asm text file at <source_path> as a binary rom <source_path>.ch8\n\n"
         << "\t./chip8 -h, --help :\n\t\t display this help." << std::endl;
     // ./chip8 -t <rom_path> ,trace mode to display register values and opcode executed each cycle
     // ./chip8 -c --compile octo.src , compile a XOchip program from octo source code
@@ -24,6 +24,8 @@ int main(int ac, char *av[]) {
             interpreter.dumpRom(av[2]);
         else if (!strcmp(av[1], "-d") || !strcmp(av[1], "--disassembly"))
             interpreter.disassemblyRom(av[2]);
+//        else if (!strcmp(av[1], "-a") || !strcmp(av[1], "--assembly"))
+//            interpreter.assemblyText(av[2]);
         else
             interpreter.loadRom(av[1]);
     } else {
